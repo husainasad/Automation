fe2da8a7e10544ee5f0a8813504bace47226d286
husainasad <shusain_be16@thapar.edu>
2020-07-13 15:25:30 +0530

    added custom message with connection request

--- Selenium/GoogleSheetSelenium/SMEReachout.py
+++ Selenium/GoogleSheetSelenium/SMEReachout.py
@@ -35,7 +35,8 @@ password = driver.find_element_by_id('password').send_keys(pw)
 login = driver.find_element_by_class_name('login__form_action_container')
 login.click()
 
-driver.get(sheet.cell(183,2).value)
+#driver.get(sheet.cell(183,2).value)
+driver.get('https://www.linkedin.com/in/sandeep-thilakan-30b7a513b/')
 
 src = driver.page_source
 soup = BeautifulSoup(src, 'lxml')
@@ -44,21 +45,29 @@ name_div = soup.find('div', {'class':'flex-1 mr5'})
 name_loc =  name_div.find_all('ul')
 name = name_loc[0].find('li').get_text().strip()
 
-exp_section = soup.find('section', {'id':'experience-section'})
-exp_section = exp_section.find('ul')
-li_tags = exp_section.find('div')
-a_tags = li_tags.find('a')
-job_title = a_tags.find('h3').get_text().strip()
-company = a_tags.find_all('p')[1].get_text().strip()
+name = name.split(" ")[0]
+
+#exp_section = soup.find('section', {'id':'experience-section'})
+#exp_section = exp_section.find('ul')
+#li_tags = exp_section.find('div')
+#a_tags = li_tags.find('a')
+#job_title = a_tags.find('h3').get_text().strip()
+#company = a_tags.find_all('p')[1].get_text().strip()
 
 edu_section = soup.find('section', {'id':'education-section'})
 university_name = edu_section.find('h3').get_text().strip()
 degree_name = edu_section.find('p',{'class':'pv-entity__secondary-title pv-entity__degree-name t-14 t-black t-normal'}).find_all('span')[1].get_text().strip()
 degree_year = edu_section.find('p',{'class':'pv-entity__dates t-14 t-black--light t-normal'}).find_all('span')[1].find_all('time')[1].get_text().strip()
-#def autofill(rowno, link):
-#    driver.get(link)
-#    name = driver.find_element_by_class_name('inline.t-24.t-black.t-normal.break-words')
-#
+
+driver.find_element_by_class_name('pv-s-profile-actions').click()
+driver.find_element_by_xpath('//button[@class="mr1 artdeco-button artdeco-button--muted artdeco-button--3 artdeco-button--secondary ember-view"]').click()
+
+custom_msg = 'Hello ' + name + '\nWe, at upGrad, are looking for SMEs for our upcoming online software development programs. We believe that it’s an ideal opportunity for us to make rigorous higher education accessible to a wider audience. This will be a part-time engagement, and we will be working remotely.Thanks!'
+msg_space = driver.find_element_by_id('custom-message')
+msg_space.send_keys(custom_msg)
+driver.find_element_by_class_name('ml1').click()
+
+
 #start = input("Enter start of row")
 #
 #cell = sheet.cell(start,2).value
