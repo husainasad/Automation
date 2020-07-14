a5c40fd6c9bc2d1f8da056f83bc5ae3cca37bf98
husainasad <shusain_be16@thapar.edu>
2020-07-15 04:43:40 +0530

    added getProfiles feature

--- Selenium/GoogleSheetSelenium/SMEReachout.py
+++ Selenium/GoogleSheetSelenium/SMEReachout.py
@@ -24,23 +24,6 @@ client = gspread.authorize(creds)
 
 sheet = client.open('SD 2.0 Tracker').worksheet('SME Pool - Reachout')
 
-query = input("Enter the area of expertise: ")
-
-emailid = input("Enter emailid")
-pw = getpass.getpass("Enter Password")
-
-driver = webdriver.Firefox(executable_path='F:\Projects\Automation\Selenium\geckodriver.exe')
-driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
-
-
-username = driver.find_element_by_id('username').send_keys(emailid)
-password = driver.find_element_by_id('password').send_keys(pw)
-
-login = driver.find_element_by_class_name('login__form_action_container')
-login.click()
-
-base_url = 'https://www.linkedin.com/search/results/people/?facetCurrentCompany=%5B%221009%22%2C%221028%22%2C%221035%22%2C%222382910%22%2C%221068%22%2C%2211174522%22%2C%221441%22%2C%221482%22%2C%221586%22%2C%22162479%22%2C%22165158%22%2C%221666%22%2C%221753%22%2C%222340144%22%2C%222646%22%2C%222751%22%2C%222988%22%2C%2231513022%22%2C%223545%22%2C%229390173%22%5D&facetGeoRegion=%5B%22in%3A0%22%5D&facetNetwork=%5B%22S%22%2C%22O%22%5D&keywords='
-driver.get(base_url + query)
 
 def next_available_row(Sheet):
     str_list = list(filter(None, Sheet.col_values(1)))
@@ -58,7 +41,7 @@ def connectMsg(Name):
     except:
         print("error in connecting")
 
-def fillSheet(new_row):
+def fillSheet(new_row,query):
     time.sleep(2)
     link = driver.current_url
     sheet.update_cell(new_row, 2, link)
@@ -74,22 +57,21 @@ def fillSheet(new_row):
         print("error in name section")
     
 
-        try:
-            exp_section = soup.find('section', {'id':'experience-section'})
-            exp_section = exp_section.find('ul')
-            li_tags = exp_section.find('div')
-            a_tags = li_tags.find('a')
-            job_title = a_tags.find('h3').get_text().strip()
-            sheet.update_cell(new_row, 20, job_title) 
+    try:
+        exp_section = soup.find('section', {'id':'experience-section'})
+        exp_section = exp_section.find('ul')
+        li_tags = exp_section.find('div')
+        a_tags = li_tags.find('a')
+        job_title = a_tags.find('h3').get_text().strip()
+        sheet.update_cell(new_row, 20, job_title) 
         
-            company = a_tags.find_all('p')[1].get_text().strip()
-            sheet.update_cell(new_row, 19, company) 
-        except:
-            profile_title = name_div.find('h2').get_text().strip()
-            profile_title = 'FPGA at honeywell'
-            job_title, company = profile_title.split(" at ")
-            sheet.update_cell(new_row, 19, company)
-            sheet.update_cell(new_row, 20, job_title)
+        company = a_tags.find_all('p')[1].get_text().strip()
+        sheet.update_cell(new_row, 19, company) 
+    except:
+        profile_title = name_div.find('h2').get_text().strip()
+        job_title, company = profile_title.split(" at ")
+        sheet.update_cell(new_row, 19, company)
+        sheet.update_cell(new_row, 20, job_title)
             
     
     try:
@@ -117,6 +99,8 @@ def fillSheet(new_row):
     
     sheet.update_cell(new_row, 7, 'Selenium')
     
+    sheet.update_cell(new_row, 23, query)
+    
     for  x in range(8,19):
         sheet.update_cell(new_row, x, 'NA')
     
@@ -126,22 +110,55 @@ def fillSheet(new_row):
     connectMsg(name)
 
 #count = 10
+#profile_Q = []
 #while count>0:
+#    profile_Q.extend(driver.find_elements_by_xpath('//span[@class = "name actor-name"]'))
 #    for person in driver.find_elements_by_xpath('//span[@class = "name actor-name"]'):
 #        person.click()
-#        try:
-#            connect = driver.find_element_by_class_name('pv-s-profile-actions')
-#            if connect.is_displayed() and connect.is_enabled():
-#                fillSheet(next_available_row(sheet))
-#                count -=1
-#        except NoSuchElementException:
-#            print ("Connect Error")
+#        time.sleep(2)
+#        fillSheet(next_available_row(sheet))
+#        driver.back()
+#        count -=1
 
-count = 10
-while count>0:
-    for person in driver.find_elements_by_xpath('//span[@class = "name actor-name"]'):
-        person.click()
-        time.sleep(2)
-        fillSheet(next_available_row(sheet))
-        driver.back()
-        count -=1
+    
+def getProfiles(profile_list ):
+    profiles = []
+    soup = BeautifulSoup(driver.page_source)
+    profile_results = soup.find('ul', {'class': 'search-results__list list-style-none'})
+    results = profile_results.find_all('li', {'class': 'search-result search-result__occluded-item ember-view'})
+    for result in results:
+        userID = result.find('a', {'class': 'search-result__result-link ember-view'}).get('href')
+        profiles.append(userID)
+    return profiles
+
+query = input("Enter the area of expertise: ")
+driver = webdriver.Firefox(executable_path='F:\Projects\Automation\Selenium\geckodriver.exe')
+driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
+
+emailid = input("Enter emailid")
+pw = getpass.getpass("Enter Password")
+    
+username = driver.find_element_by_id('username').send_keys(emailid)
+password = driver.find_element_by_id('password').send_keys(pw)
+
+
+login = driver.find_element_by_class_name('login__form_action_container')
+login.click()
+
+base_url = 'https://www.linkedin.com/search/results/people/?facetCurrentCompany=%5B%221009%22%2C%221028%22%2C%221035%22%2C%222382910%22%2C%221068%22%2C%2211174522%22%2C%221441%22%2C%221482%22%2C%221586%22%2C%22162479%22%2C%22165158%22%2C%221666%22%2C%221753%22%2C%222340144%22%2C%222646%22%2C%222751%22%2C%222988%22%2C%2231513022%22%2C%223545%22%2C%229390173%22%5D&facetGeoRegion=%5B%22in%3A0%22%5D&facetNetwork=%5B%22S%22%2C%22O%22%5D&keywords='
+page_no = '1'
+driver.get(base_url + query + '&page=' + page_no )
+driver.implicitly_wait(5)
+
+
+profile_list = []
+
+profile_list = getProfiles(profile_list)
+
+while profile_list:
+    profile = profile_list.pop()
+    profile_link = 'https://www.linkedin.com/' + profile
+    driver.get(profile_link)
+    fillSheet(next_available_row(sheet), query)
+    
+#driver.quit()
